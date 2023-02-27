86fc89ff7ac0a8f927e5f63cd82fb464d5ab2efd
SergeiShilkoysky <sergshrb79@gmail.com>
2023-02-27 08:20:39 +0300

    №36_В массиве найти сумму  элементов стоящих на нечетных позициях

--- Hw_1/Program.cs
+++ Hw_1/Program.cs
@@ -2,3 +2,32 @@
 // Напишите программу, которая покажет количество чётных чисел в массиве. 
 // [345, 897, 568, 234] -> 2
 
+
+int[] arrayInit(int size)
+{
+    int[] array = new int[size];
+    Random rnd = new Random();
+    for (int i = 0; i < array.Length; i++)
+    {
+        array[i] = rnd.Next(100, 999);
+    }
+    return array;
+}
+
+int checkEvenNumber(int[] array)
+{
+    int result = 0;
+    for (int i = 0; i < array.Length; i++)
+    {
+        if (array[i] % 2 == 0) result++;
+    }
+    return result;
+}
+void print(int[] array)
+{
+    Console.WriteLine($"[{string.Join(",", array)}]");
+}
+
+int[] array = arrayInit(6);
+print(array);
+Console.WriteLine($"в указанном массиве колличество четных чисел = {checkEvenNumber(array)}");
--- Hw_2/Program.cs
+++ Hw_2/Program.cs
@@ -2,3 +2,32 @@
 // [3, 7, 23, 12] -> 19  /  [-4, -6, 89, 6] -> 0
 
 
+int[] arrayInit(int size)
+{
+    int[] array = new int[size];
+    Random rnd = new Random();
+    for (int i = 0; i < array.Length; i++)
+    {
+        array[i] = rnd.Next(0, 20);
+    }
+    return array;
+}
+
+int sumElementOddPosition(int[] array)
+{
+    int result = 0;
+    for (int i = 1; i < array.Length; i+=2)
+    {
+       // if (array[i] % 2 == 1) result++;
+       result += array[i];
+    }
+    return result;
+}
+void print(int[] array)
+{
+    Console.WriteLine($"[{string.Join(",", array)}]");
+}
+
+int[] array = arrayInit(6);
+print(array);
+Console.WriteLine($"в указанном массиве сумма элементов на нечетных позициях = {sumElementOddPosition(array)}");
